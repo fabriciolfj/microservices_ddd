ac17fe82c165944e88d71876a98fe4113e589fa3
fabricio <fabricio.jacob@outlook.com>
2020-05-25 16:57:46 -0300

    add

--- microservices/product/src/main/java/br/com/spark/service/product/api/dto/request/ProductRequestMapperDecorator.java
+++ microservices/product/src/main/java/br/com/spark/service/product/api/dto/request/ProductRequestMapperDecorator.java
@@ -1,6 +1,7 @@
 package br.com.spark.service.product.api.dto.request;
 
 import br.com.spark.service.product.domain.model.Product;
+import br.com.spark.service.product.domain.model.enuns.ProductStatus;
 import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -13,6 +14,7 @@ public abstract class ProductRequestMapperDecorator implements ProductRequestMap
     @Override
     public Product toDomain(ProductRequestDto dto) {
         var product = mapper.toDomain(dto);
-        return null;
+        product.setStatus(ProductStatus.toEnum(dto.getStatus()));
+        return product;
     }
 }
